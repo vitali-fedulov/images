489aae061eddb41c500d57fea61d93152419c2f8
Vitali Fedulov <fedulov.vitali@gmail.com>
2018-11-09 21:48:01 +0100

    Package description

--- open.go
+++ open.go
@@ -1,7 +1,7 @@
 // Copyright 2018 Vitali Fedulov. All rights reserved. Use of this source code
 // is governed by a MIT-style license that can be found in the LICENSE file.
 
-// Package images is a program for image comparison by perceptual similarity.
+// Package images allows image comparison by perceptual similarity.
 // Supported image types are those default to the Go image package
 // https://golang.org/pkg/image/ (which are GIF, JPEG and PNG in October 2018).
 package images
